0d32b7d100545c3eefde4420c77d7ca8d67b5dbd
karined19 <65109427+karined19@users.noreply.github.com>
2026-02-18 12:59:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,35 @@
 import streamlit as st
 
-st.title("🎉 Mon app fonctionne !")
-st.write("✨ Ceci est ma première modification en direct !")
+# ----- CSS -----
+st.markdown("""
+<style>
 
+body {
+    background-color: #f7f9fc;
+}
+
+h1 {
+    color: #6C63FF;
+    text-align: center;
+}
+
+.stButton > button {
+    border-radius: 12px;
+    background-color: #6C63FF;
+    color: white;
+    font-weight: bold;
+}
+
+</style>
+""", unsafe_allow_html=True)
+
+# ----- APP -----
+st.title("🧭 Mon assistant d'aides")
+
+st.write("Bienvenue dans ton prototype 🙂")
+
+name = st.text_input("Ton prénom")
+
+if name:
+    st.success(f"Bonjour {name} !")
 
